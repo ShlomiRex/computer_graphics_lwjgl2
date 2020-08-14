cb89cf092cb30ca344920f643ff94eeb351bdd86
Shlomi Haver <shlomi.haver@booking.com>
2020-08-14 17:55:21 +0300

    add spotlight

--- src/Engine/Light/SpatialLight.java
+++ src/Engine/Light/SpatialLight.java
@@ -4,6 +4,7 @@ import Engine.EngineObject.ISpatialObject;
 import Engine.EngineObject.SpatialObject;
 import Engine.EngineObject.Sphere;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.util.vector.Vector3f;
 import org.lwjgl.util.vector.Vector4f;
 
 import java.nio.FloatBuffer;
@@ -30,22 +31,21 @@ public class SpatialLight extends Light implements ISpatialObject {
     /**
      * The rendered spatial object of this light.
      */
-    protected SpatialObject renderObject;
+    protected Sphere renderObject;
 
     public SpatialLight(int light_number, boolean is_directional) {
         super(light_number);
         position = new Vector4f(0, 0, 0, is_directional ? 0 : 1);
         renderObject = new Sphere();
 
-        renderObject.name = "Light " + ((light_number / GL_LIGHT0) - 1);
+        renderObject.setName("Light " + ((light_number / GL_LIGHT0) - 1));
 
-        renderObject.color.x = 0.5f;
-        renderObject.color.y = 0.5f;
-        renderObject.color.z = 0f;
+        renderObject.setColor(new Vector4f(0.5f, 0.5f, 0, 1));
+        float lightScale = 0.1f;
+        renderObject.setScale(new Vector3f(lightScale, lightScale, lightScale));
     }
 
     /**
-     *
      * @return positional buffer for glLight(GL_LIGHTX, GL_POSITION, buffer)
      */
     public FloatBuffer getPositionFloatBuffer() {
@@ -56,12 +56,12 @@ public class SpatialLight extends Light implements ISpatialObject {
 
     @Override
     public void update() {
-        if(updateNeeded == false)
+        if (updateNeeded == false)
             return;
 
         glLight(LIGHT_X, GL_POSITION, getPositionFloatBuffer());
 
-        if(enabled == false)
+        if (enabled == false)
             glDisable(LIGHT_X);
         else
             glEnable(LIGHT_X);
@@ -71,16 +71,16 @@ public class SpatialLight extends Light implements ISpatialObject {
 
     @Override
     public void render() {
-        if(toRender == false || glIsEnabled(LIGHT_X) == false)
+        if (toRender == false || glIsEnabled(LIGHT_X) == false)
             return;
 
         renderObject.position.x = position.x;
         renderObject.position.y = position.y;
         renderObject.position.z = position.z;
 
-        float materialColor_red = Light.DEFAULT_AMBIENT.getX() * renderObject.color.x;
-        float materialColor_green = Light.DEFAULT_AMBIENT.getY() * renderObject.color.y;
-        float materialColor_blue = Light.DEFAULT_AMBIENT.getZ() * renderObject.color.z;
+        float materialColor_red = Light.DEFAULT_AMBIENT.getX() * renderObject.getColor().x;
+        float materialColor_green = Light.DEFAULT_AMBIENT.getY() * renderObject.getColor().y;
+        float materialColor_blue = Light.DEFAULT_AMBIENT.getZ() * renderObject.getColor().z;
 
         glColor3f(materialColor_red, materialColor_green, materialColor_blue);
         renderObject.render();
